11dc658b52388aede50f190a3b36d7eba53f428c
sahilbawa <sahilbawa@users.noreply.github.com>
2021-09-20 22:44:21 -0700

    API link update

--- Vet Dashboard/Models/DashboardDAL.cs
+++ Vet Dashboard/Models/DashboardDAL.cs
@@ -12,7 +12,7 @@ namespace Vet_Dashboard.Models
 {
     public class DashboardDAL
     {
-        private static string _apiAppointmentsUrl = "https://api.sahilbawa.com/vetdashboard";
+        private static string _apiAppointmentsUrl = "https://api.sahilbawa.com/appointments/default";
         private readonly HttpClient _client;
 
         public DashboardDAL()
